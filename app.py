7d2c182b6a10a63041ea760afbcef98d92502e69
rkaus <rkaus1024@gmail.com>
2018-01-19 17:05:36 -0800

    uuid session tracking

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask import (request,
+                   redirect,
+                   url_for,
+                   session,
+                   render_template)
+
+from uuid import uuid1
+
 
 app = Flask(__name__,static_url_path='')
 # app = Flask(__name__)
 
 
+def create_user():
+    user = session.get('user','not a user')
+    if user == 'not a user':
+        session['user'] = str(uuid1())
+		session.permanent = True
+
+
+
 @app.route('/')
 def index():
+
+	if 'user' not in session.keys():
+		create_user()
     # return app.send_static_file('index.html')
     return render_template('index.html')
 
@@ -22,3 +41,4 @@ def index():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
+    
